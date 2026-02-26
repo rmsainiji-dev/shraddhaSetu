03e708beb639f6a7403fa4ad4e2ca7cafd04f7c0
Ramesh <rmsainiji@gmail.com>
2026-02-26 19:42:53 +0530

    next.config.js change for github pages

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  // Required for GitHub Pages if deploying to a subdirectory
-  // basePath: '/shraddhaSetu', // Uncomment and set your repo name for GitHub Pages
+  basePath: isProd ? "/shraddhaSetu" : "",
 };
 
 module.exports = nextConfig;
