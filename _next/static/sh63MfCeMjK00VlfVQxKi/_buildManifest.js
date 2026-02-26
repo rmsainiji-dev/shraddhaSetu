e9cdab78a6d6bd3c72a0f2e5769e012a33f2e68a
Ramesh <rmsainiji@gmail.com>
2026-02-26 19:53:14 +0530

    Updates

--- _next/static/sh63MfCeMjK00VlfVQxKi/_buildManifest.js
+++ _next/static/sh63MfCeMjK00VlfVQxKi/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/shraddhaSetu//_next/:path+",
+        "destination": "/shraddhaSetu/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
